55399303f708d1e1533213f78132acb52b386192
Tahim Mondal <133028260+Tahimpro@users.noreply.github.com>
2025-02-08 04:37:04 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -5,12 +5,15 @@
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from info import CHANNELS, MOVIE_UPDATE_CHANNEL, ADMINS, LOG_CHANNEL
+from info import CHANNELS, MOVIE_UPDATE_CHANNEL, ADMINS , LOG_CHANNEL
 from database.ia_filterdb import save_file, unpack_new_file_id
+from utils import get_poster, temp
 import re
 from database.users_chats_db import db
 
 processed_movies = set()
+media_filter = filters.document | filters.video
+
 media_filter = filters.document | filters.video
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
@@ -25,9 +28,16 @@ async def media(bot, message):
             file_id, file_ref = unpack_new_file_id(media.file_id)
             await send_movie_updates(bot, file_name=media.file_name, caption=media.caption, file_id=file_id)
 
+async def get_imdb(file_name):
+    imdb_file_name = await movie_name_format(file_name)
+    imdb = await get_poster(imdb_file_name)
+    if imdb:
+        return imdb.get('poster')
+    return None
+
 async def movie_name_format(file_name):
-    filename = re.sub(r'http\S+', '', re.sub(r'@\w+|#\w+', '', file_name).replace('_', ' ').replace('[', '').replace(']', '').replace('(', '').replace(')', '').replace('{', '').replace('}', '').replace('.', ' ').replace('@', '').replace(':', '').replace(';', '').replace("'", '').replace('-', '').replace('!', '')).strip()
-    return filename
+  filename = re.sub(r'http\S+', '', re.sub(r'@\w+|#\w+', '', file_name).replace('_', ' ').replace('[', '').replace(']', '').replace('(', '').replace(')', '').replace('{', '').replace('}', '').replace('.', ' ').replace('@', '').replace(':', '').replace(';', '').replace("'", '').replace('-', '').replace('!', '')).strip()
+  return filename
 
 async def check_qualities(text, qualities: list):
     quality = []
@@ -52,7 +62,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
                 season = season.group(1) if season else None       
                 file_name = file_name[:file_name.find(season) + 1]
         qualities = ["ORG", "org", "hdcam", "HDCAM", "HQ", "hq", "HDRip", "hdrip", 
-                     "camrip", "WEB-DL", "CAMRip", "hdtc", "predvd", "DVDscr", "dvdscr", 
+                     "camrip", "WEB-DL" "CAMRip", "hdtc", "predvd", "DVDscr", "dvdscr", 
                      "dvdrip", "dvdscr", "HDTC", "dvdscreen", "HDTS", "hdts"]
         quality = await check_qualities(caption, qualities) or "HDRip"
         language = ""
@@ -65,7 +75,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         if movie_name in processed_movies:
             return 
         processed_movies.add(movie_name)    
-        
+        poster_url = await get_imdb(movie_name)
         caption_message = f"<b><i>#𝖬𝗈𝗏𝗂𝖾/𝖲𝖾𝗋𝗂𝖾𝗌-𝖴𝗉𝖽𝖺𝗍𝖾✅</i>\n\n🎬 𝖭𝖺𝗆𝖾:- <code>{movie_name}</code>\n\n<blockquote>🎙️ 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾:- {language}\n\n🎚️ 𝖰𝗎𝖺𝗅𝗂𝗍𝗒:- {quality}</blockquote></b>" 
         search_movie = movie_name.replace(" ", '-')
         movie_update_channel = await db.movies_update_channel_id()    
@@ -75,11 +85,14 @@ async def send_movie_updates(bot, file_name, caption, file_id):
             InlineKeyboardButton('📥 𝖧𝗈𝗐 𝖳𝗈 𝖣𝗈𝗐𝗇𝗅𝗈𝖺𝖽 📥', url=f'https://t.me/How2download_Cpflix_Files/4')
         ]]
         reply_markup = InlineKeyboardMarkup(btn)
-
-        # Send only the text message without a poster
-        await bot.send_message(movie_update_channel if movie_update_channel else MOVIE_UPDATE_CHANNEL, 
-                               text=caption_message, reply_markup=reply_markup)
-
+        if poster_url:
+            await bot.send_photo(movie_update_channel if movie_update_channel else MOVIE_UPDATE_CHANNEL, 
+                                 photo=poster_url, caption=caption_message, reply_markup=reply_markup)
+        else:
+            no_poster = "https://telegra.ph/file/88d845b4f8a024a71465d.jpg"
+            await bot.send_photo(movie_update_channel if movie_update_channel else MOVIE_UPDATE_CHANNEL, 
+                                 photo=no_poster, caption=caption_message, reply_markup=reply_markup)  
     except Exception as e:
         print('Failed to send movie update. Error - ', e)
         await bot.send_message(LOG_CHANNEL, f'Failed to send movie update. Error - {e}')
+
