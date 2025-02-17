33007a2be65ed23ef6ed89bd241a3febec2954af
Tahim Mondal <133028260+Tahimpro@users.noreply.github.com>
2025-02-17 23:10:17 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -110,10 +110,10 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         processed_movies.add(movie_name)
 
         caption_message = (
-            f"<b>#𝖬𝗈𝗏𝗂𝖾/𝖲𝖾𝗋𝗂𝖾𝗌-𝖴𝗉𝖽𝖺𝗍𝖾✅\n\n"
-            f"🎬 𝖭𝖺𝗆𝖾:- {movie_name}\n\n"
-            f"<blockquote>🎙️ 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾:- #{language}\n\n"
-            f"🎚️ 𝖰𝗎𝖺𝗅𝗂𝗍𝗒:- #{quality}</blockquote></b>"
+            f"<b><u>𝗧𝗢𝗗𝗔𝗬 𝗨𝗣𝗗𝗔𝗧𝗘𝗦✅</u>\n\n"
+            f"🎬 {movie_name}\n\n"
+            f"<blockquote>🎙️{language}\n\n"
+            f"📺{quality}</blockquote></b>"
         )
 
         search_movie = movie_name.replace(" ", '-')
